3eef5cbab0fd929bfb58dcc452fe1bd6e42f6bb7
davidabrownell <davidabrownell@gmail.com>
2020-01-29 12:45:17 -0800

    hello world test

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import YelpReview from './app';
+const http = require('http');
 
-const application = new YelpReview();
-application.listen(3000);
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
