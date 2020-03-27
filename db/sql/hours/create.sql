9c4290a3e13c2f14921eb1a0a8037a445e0ab10b
David Brownell <davidabrownell@gmail.com>
2020-03-27 12:04:01 -0700

    Changed Hours schema

--- db/sql/hours/create.sql
+++ db/sql/hours/create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Hours (
   business    CHAR(22)   REFERENCES Business(id),
-  day_of_week VARCHAR    NOT NULL,
-  open        VARCHAR  NOT NULL,
-  close       VARCHAR  NOT NULL,
+  day_of_week VARCHAR,
+  open        TIME       NOT NULL,
+  close       TIME       NOT NULL,
+
   PRIMARY KEY (business, day_of_week)
 );
