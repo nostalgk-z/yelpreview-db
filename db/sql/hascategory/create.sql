fdb7ab80d20dd12dd0ea31c59696aa2cdffde65e
ZanePartain <zane.partain@wsu.edu>
2020-03-27 16:16:18 -0700

    change hascat

--- db/sql/hascategory/create.sql
+++ db/sql/hascategory/create.sql
@@ -1,6 +1,6 @@
 /*Many to Many between Business and Category*/
 CREATE TABLE IF NOT EXISTS HasCategory (
 	business CHAR(22) REFERENCES Business(id),
-	category INTEGER  REFERENCES Category(id),
+	category TEXT  REFERENCES Category(type),
 	PRIMARY KEY (business,category) -- implied cannot be null
 );
