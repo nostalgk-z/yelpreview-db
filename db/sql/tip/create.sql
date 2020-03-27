c7ec99ba956d533ce31254d52ddf813ee5000d65
David Brownell <davidabrownell@gmail.com>
2020-03-27 16:11:51 -0700

    Changed Tip.id to SERIAL

--- db/sql/tip/create.sql
+++ db/sql/tip/create.sql
@@ -1,6 +1,6 @@
 /* Ternary relation involving Business and User [[ENTITY]] */
 CREATE TABLE IF NOT EXISTS Tip (
-	id       CHAR(22)  PRIMARY KEY,
+	id       SERIAL    PRIMARY KEY,
 	business CHAR(22)  REFERENCES Business(id),
 	user_id  CHAR(22)  REFERENCES Users(id),
 	date     TIMESTAMP NOT NULL,
