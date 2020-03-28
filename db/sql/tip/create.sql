1ccb7ced1ab55ba323618e0a52bb463d30368ba7
David Brownell <davidabrownell@gmail.com>
2020-03-28 15:07:55 -0700

    Added triggers for CheckIn and Tip.

--- db/sql/tip/create.sql
+++ db/sql/tip/create.sql
@@ -7,3 +7,70 @@ CREATE TABLE IF NOT EXISTS Tip (
 	text     TEXT      NOT NULL,
 	likes    INTEGER   NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION UpdateTip() RETURNS TRIGGER AS $UpdateTip$
+    BEGIN
+        IF (TG_OP = 'INSERT') THEN
+            UPDATE business
+            SET num_tips = num_tips + 1
+            WHERE business.id = NEW.business;
+
+            UPDATE users
+            SET tip_count = tip_count + 1
+            WHERE users.id = NEW.user_id;
+
+            UPDATE users
+            SET total_likes = total_likes + NEW.likes
+            WHERE users.id = NEW.user_id;
+
+            RETURN NEW;
+        ELSEIF (TG_OP = 'DELETE') THEN
+            UPDATE business
+            SET num_tips = GREATEST(0, num_tips - 1)
+            WHERE business.id = OLD.business;
+
+            UPDATE users
+            SET tip_count = GREATEST(0, tip_count - 1)
+            WHERE users.id = OLD.user_id;
+
+            UPDATE users
+            SET total_likes = total_likes - OLD.likes
+            WHERE users.id = NEW.user_id;
+            
+            RETURN OLD;
+        ELSEIF (TG_OP = 'UPDATE') THEN
+            UPDATE business
+            SET num_tips = GREATEST(0, num_tips - 1)
+            WHERE business.id = OLD.business;
+
+            UPDATE business
+            SET num_tips = num_tips + 1
+            WHERE business.id = NEW.business;
+
+            UPDATE users
+            SET tip_count = GREATEST(0, tip_count - 1)
+            WHERE users.id = OLD.user_id;
+            
+            UPDATE users
+            SET tip_count = tip_count + 1
+            WHERE users.id = NEW.user_id;
+
+            UPDATE users
+            SET total_likes = total_likes - OLD.likes
+            WHERE users.id = OLD.user_id;
+
+            UPDATE users
+            SET total_likes = total_likes + NEW.likes
+            WHERE users.id = NEW.user_id;
+
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$UpdateTip$ LANGUAGE plpgsql;
+
+CREATE TRIGGER CheckInInc
+    AFTER INSERT OR UPDATE OR DELETE ON tip
+    FOR EACH ROW
+EXECUTE PROCEDURE UpdateTip();
+
